69fe2cacf442765190dd2b9ebed1406a8da6f084
Jake Rhodes <jakerhodes8@gmail.com>
2025-06-17 18:25:42 -0600

    add in regression and classification methods

--- methods/__init__.py
+++ methods/__init__.py
@@ -1,11 +1,17 @@
 # Import all methods, broken down by classification and regression
 
-from .nonconformity import nonconformity_rfgap, nonconformity_original, nonconformity_oob
+from .get_regression_results import get_qrf_results, get_zrf_results, get_rf_results
+from .get_classification_results import get_classification_results
 
 
+classification_methods = {
+    'rf': get_classification_results,
+}
 
-classification_methods = {}
-
-regression_methods = {}
+regression_methods = {
+    'qrf': get_qrf_results,
+    'zrf': get_zrf_results,
+    'rf': get_rf_results
+}
 
 
